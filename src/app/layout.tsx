3c2043fd2b3a012ec5ab30d2bcf3e0b13981ae86
Phil Wolstenholme <philip.wolstenholme@coop.co.uk>
2023-10-20 15:16:20 +0100

    add GTM component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleTagManager gtmId="GTM-TDHP87R" />
     </html>
   )
 }
